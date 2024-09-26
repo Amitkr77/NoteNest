7469db2465951479994148492ee38eeb2df1d259
Amit kumar <amitroyk00@gmail.com>
2024-09-26 12:00:22 +0530

    Prepare for Heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,19 @@ import cors from 'cors';
 import profileRoutes from './src/routes/profileRoutes.js';
 import workItemRoutes from './src/routes/workItemRoutes.js';
 
+const path = require('path');
 const app = express();
 const port = 8001;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// The "catchall" handler: for any request that doesn't match one above,
+// send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
+});
+
 
 // Middleware
 app.use(express.json());
