6847bcf783ab7a5c8f1cc173a007f77362ed8b73
Amitkr77 <amitroyk99@gmail.com>
2024-10-14 22:13:25 +0530

    test

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const Header = () => (
   <div className="w-full my-8">
     <h2 className="text-center text-3xl mb-4">
-      -Note<span className="text-custom-orange">Nest</span>-
+      -Note<span className="text-orange-500">Nest</span>-
     </h2>
+    <marquee >The NoteNest is workbook appliaction where you can save work with discription in the database</marquee>
   </div>
 );
 
