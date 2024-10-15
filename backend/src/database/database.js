4df2517c3387522b9e965af98025ce2c550e6be8
Amitkr77 <amitroyk99@gmail.com>
2024-10-15 23:48:49 +0530

    updated string

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const db =async()=>{
     try {
-        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/workBook')
-        console.log(`MongoDb connected !! DB HOST : ${conn.connection.host}`);
+        const conn = await mongoose.connect('mongodb+srv://amitroyk99:NoteNest@cluster0.xvwrs.mongodb.net/workbook')
+        console.log(`MongoDb connected !! DB HOST : ${conn.connection.host} ${conn.connection.name}`);
         
     } catch (error) {
         console.error("Mongoose connection error", error);
