42ee4278efe4dd2bb9deab66386ecb84b98ac012
Amitkr77 <amitroyk99@gmail.com>
2024-10-15 09:15:29 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -4,29 +4,19 @@ import cors from 'cors';
 import profileRoutes from './src/routes/profileRoutes.js';
 import workItemRoutes from './src/routes/workItemRoutes.js';
 import path from "path";
-import { fileURLToPath } from 'url';
 
 // Initialize the Express application
 const app = express();
 const PORT = 8001;
 
-// Get the directory name for the current module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // Middleware configuration
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Route configuration
 app.use('/api/profiles', profileRoutes);
 app.use('/api/workitems', workItemRoutes);
 
-// Catch-all handler for React
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
 
 // Database connection
 db();
